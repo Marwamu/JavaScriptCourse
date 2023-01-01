be00b7a0d7a0c279e1db5a7a8aa990cd7f49d47f
Marwamu <marwamohamed1293@gmail.com>
2023-01-01 16:25:28 +0200

    create calculate button

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 var ageButton = document.getElementById('bt-age');
 var printage = document.getElementById('print-age');
-
+var yearInput = document.getElementById('input-year');
+var errorMessage = document.getElementById('error');
 
 // var year = 1998;
 // var age = calc(year);
 // console.log(age);
 ageButton.addEventListener('click', () => {
-    var yearInput = document.getElementById('input-year').value;
-    const age = 2023 - yearInput;
-    // console.log(age);
-    printage.innerHTML = age;
+    console.log(yearInput.value);
+    if (yearInput.value == '' || yearInput.value == '0') {
+        console.log(errorMessage);
+        errorMessage.style.display = 'block';
+    }
+    else {
+        const age = 2023 - yearInput.value;
+        // console.log(age);
+        printage.innerHTML = age;
+        ageButton.style.backgroundColor = '#0000ff';
+        ageButton.style.color = '#ffff';
+        errorMessage.style.display = 'none';
+    }
+
 })
